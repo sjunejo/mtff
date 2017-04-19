73f95fa0d00a1356da4e665492ca1e313667c617
sjunejo <sjunejo1@gmail.com>
2017-04-19 22:43:12 +0100

    fixed untracked files

---
+++ app/src/main/java/sjunejo/com/musictastefriendfinder/utils/Constants.java
@@ -0,0 +1,10 @@
+package sjunejo.com.musictastefriendfinder.utils;
+
+/**
+ * Created by sjunjo on 17/04/2017.
+ */
+
+public class Constants {
+
+    public static final String TAG_SJ_LOCATION = "TAG_SJ_LOCATION";
+}
